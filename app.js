276f85e4506ebc405661d991dbdf5c5fbd2a7f0c
Aditya Raj <adityaraj6220@gmail.com>
2024-02-15 00:16:01 +0530

    File updated

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.get('/',(req,res)=>{
     res.send("Hello World!");
 })
 
-
+app.get('/action', (req,res)=>{
+    res.send("Successful deployment from Github Action")
+})
 
 app.listen(8080,()=>{
     console.log("Server running on PORT:8080");
